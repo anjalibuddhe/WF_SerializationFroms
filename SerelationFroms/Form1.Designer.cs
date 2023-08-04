e1b86e19428f347e98ca273132efe7a129cb79c1
anjali buddhe <anjalibuddhe124@gmail.com>
2023-08-04 18:37:22 +0530

    Add project files.

---
+++ SerelationFroms/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SerelationFroms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Product_id = new System.Windows.Forms.Label();
+            this.Product_name = new System.Windows.Forms.Label();
+            this.Productp = new System.Windows.Forms.Label();
+            this.ProductId = new System.Windows.Forms.TextBox();
+            this.Productname = new System.Windows.Forms.TextBox();
+            this.ProductPrice = new System.Windows.Forms.TextBox();
+            this.BinaryWrite = new System.Windows.Forms.Button();
+            this.BinarRead = new System.Windows.Forms.Button();
+            this.XmlRead = new System.Windows.Forms.Button();
+            this.xmlWrite = new System.Windows.Forms.Button();
+            this.SoapWrite = new System.Windows.Forms.Button();
+            this.SOAPRead = new System.Windows.Forms.Button();
+            this.JsonRead = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Product_id
+            // 
+            this.Product_id.AutoSize = true;
+            this.Product_id.Location = new System.Drawing.Point(84, 51);
+            this.Product_id.Name = "Product_id";
+            this.Product_id.Size = new System.Drawing.Size(58, 13);
+            this.Product_id.TabIndex = 0;
+            this.Product_id.Text = "Product_id";
+            // 
+            // Product_name
+            // 
+            this.Product_name.AutoSize = true;
+            this.Product_name.Location = new System.Drawing.Point(84, 90);
+            this.Product_name.Name = "Product_name";
+            this.Product_name.Size = new System.Drawing.Size(78, 13);
+            this.Product_name.TabIndex = 1;
+            this.Product_name.Text = "Product_Name";
+            // 
+            // Productp
+            // 
+            this.Productp.AutoSize = true;
+            this.Productp.Location = new System.Drawing.Point(84, 131);
+            this.Productp.Name = "Productp";
+            this.Productp.Size = new System.Drawing.Size(73, 13);
+            this.Productp.TabIndex = 2;
+            this.Productp.Text = "Product_price";
+            // 
+            // ProductId
+            // 
+            this.ProductId.Location = new System.Drawing.Point(188, 44);
+            this.ProductId.Name = "ProductId";
+            this.ProductId.Size = new System.Drawing.Size(100, 20);
+            this.ProductId.TabIndex = 3;
+            // 
+            // Productname
+            // 
+            this.Productname.Location = new System.Drawing.Point(188, 90);
+            this.Productname.Name = "Productname";
+            this.Productname.Size = new System.Drawing.Size(100, 20);
+            this.Productname.TabIndex = 4;
+            // 
+            // ProductPrice
+            // 
+            this.ProductPrice.Location = new System.Drawing.Point(188, 131);
+            this.ProductPrice.Name = "ProductPrice";
+            this.ProductPrice.Size = new System.Drawing.Size(100, 20);
+            this.ProductPrice.TabIndex = 5;
+            // 
+            // BinaryWrite
+            // 
+            this.BinaryWrite.Location = new System.Drawing.Point(404, 46);
+            this.BinaryWrite.Name = "BinaryWrite";
+            this.BinaryWrite.Size = new System.Drawing.Size(75, 23);
+            this.BinaryWrite.TabIndex = 6;
+            this.BinaryWrite.Text = "BinaryWrite";
+            this.BinaryWrite.UseVisualStyleBackColor = true;
+            this.BinaryWrite.Click += new System.EventHandler(this.BinaryWrite_Click);
+            // 
+            // BinarRead
+            // 
+            this.BinarRead.Location = new System.Drawing.Point(507, 46);
+            this.BinarRead.Name = "BinarRead";
+            this.BinarRead.Size = new System.Drawing.Size(75, 23);
+            this.BinarRead.TabIndex = 7;
+            this.BinarRead.Text = "BinaryRead";
+            this.BinarRead.UseVisualStyleBackColor = true;
+            this.BinarRead.Click += new System.EventHandler(this.BinarRead_Click);
+            // 
+            // XmlRead
+            // 
+            this.XmlRead.Location = new System.Drawing.Point(507, 85);
+            this.XmlRead.Name = "XmlRead";
+            this.XmlRead.Size = new System.Drawing.Size(75, 23);
+            this.XmlRead.TabIndex = 8;
+            this.XmlRead.Text = "XmlRead";
+            this.XmlRead.UseVisualStyleBackColor = true;
+            this.XmlRead.Click += new System.EventHandler(this.XmlRead_Click);
+            // 
+            // xmlWrite
+            // 
+            this.xmlWrite.Location = new System.Drawing.Point(404, 88);
+            this.xmlWrite.Name = "xmlWrite";
+            this.xmlWrite.Size = new System.Drawing.Size(75, 23);
+            this.xmlWrite.TabIndex = 9;
+            this.xmlWrite.Text = "XMLWrite";
+            this.xmlWrite.UseVisualStyleBackColor = true;
+            this.xmlWrite.Click += new System.EventHandler(this.xmlWrite_Click);
+            // 
+            // SoapWrite
+            // 
+            this.SoapWrite.Location = new System.Drawing.Point(404, 131);
+            this.SoapWrite.Name = "SoapWrite";
+            this.SoapWrite.Size = new System.Drawing.Size(75, 23);
+            this.SoapWrite.TabIndex = 10;
+            this.SoapWrite.Text = "SOAPWrite";
+            this.SoapWrite.UseVisualStyleBackColor = true;
+            this.SoapWrite.Click += new System.EventHandler(this.SoapWrite_Click);
+            // 
+            // SOAPRead
+            // 
+            this.SOAPRead.Location = new System.Drawing.Point(507, 131);
+            this.SOAPRead.Name = "SOAPRead";
+            this.SOAPRead.Size = new System.Drawing.Size(75, 23);
+            this.SOAPRead.TabIndex = 11;
+            this.SOAPRead.Text = "SOAPRead";
+            this.SOAPRead.UseVisualStyleBackColor = true;
+            this.SOAPRead.Click += new System.EventHandler(this.SOAPRead_Click);
+            // 
+            // JsonRead
+            // 
+            this.JsonRead.Location = new System.Drawing.Point(404, 179);
+            this.JsonRead.Name = "JsonRead";
+            this.JsonRead.Size = new System.Drawing.Size(75, 23);
+            this.JsonRead.TabIndex = 12;
+            this.JsonRead.Text = "JSONWrite";
+            this.JsonRead.UseVisualStyleBackColor = true;
+            this.JsonRead.Click += new System.EventHandler(this.JsonRead_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(507, 179);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "JSONRead";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.JsonRead);
+            this.Controls.Add(this.SOAPRead);
+            this.Controls.Add(this.SoapWrite);
+            this.Controls.Add(this.xmlWrite);
+            this.Controls.Add(this.XmlRead);
+            this.Controls.Add(this.BinarRead);
+            this.Controls.Add(this.BinaryWrite);
+            this.Controls.Add(this.ProductPrice);
+            this.Controls.Add(this.Productname);
+            this.Controls.Add(this.ProductId);
+            this.Controls.Add(this.Productp);
+            this.Controls.Add(this.Product_name);
+            this.Controls.Add(this.Product_id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Product_id;
+        private System.Windows.Forms.Label Product_name;
+        private System.Windows.Forms.Label Productp;
+        private System.Windows.Forms.TextBox ProductId;
+        private System.Windows.Forms.TextBox Productname;
+        private System.Windows.Forms.TextBox ProductPrice;
+        private System.Windows.Forms.Button BinaryWrite;
+        private System.Windows.Forms.Button BinarRead;
+        private System.Windows.Forms.Button XmlRead;
+        private System.Windows.Forms.Button xmlWrite;
+        private System.Windows.Forms.Button SoapWrite;
+        private System.Windows.Forms.Button SOAPRead;
+        private System.Windows.Forms.Button JsonRead;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ SerelationFroms/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Linq;
+//XML Ser
+using System.Xml.Serialization;
+//SOAP
+using System.Runtime.Serialization.Formatters.Soap;
+//JSON
+using System.Text.Json;
+
+namespace SerelationFroms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.dat";
+                FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+                BinaryFormatter formatter = new BinaryFormatter();
+                Product p1 = new Product();
+                p1.Id = Convert.ToInt32(ProductId.Text);
+                p1.Name = Productname.Text;
+                p1.Price = Convert.ToInt32(ProductPrice.Text);
+                formatter.Serialize(fileStream, p1);
+                fileStream.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void BinarRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.dat";
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                BinaryFormatter formatter = new BinaryFormatter();
+                Product p1 = new Product();
+                // we done explicit type casting from Object --> Product
+                p1 = (Product)formatter.Deserialize(fileStream);
+                ProductId.Text = p1.Id.ToString();
+                Productname.Text = p1.Name;
+                ProductPrice.Text = p1.Price.ToString();
+
+                fileStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void xmlWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.xml";
+                FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
+                Product p1 = new Product();
+                p1.Id = Convert.ToInt32(ProductId.Text);
+                p1.Name = Productname.Text;
+                p1.Price = Convert.ToInt32(ProductPrice.Text);
+                xmlSerializer.Serialize(fileStream, p1);
+                fileStream.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void XmlRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.xml";
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                XmlSerializer xmlSerializer= new XmlSerializer(typeof(Product));    
+                Product p1 = new Product();
+                // we done explicit type casting from Object --> Product
+                p1 = (Product)xmlSerializer.Deserialize(fileStream);
+                ProductId.Text = p1.Id.ToString();
+                Productname.Text = p1.Name;
+                ProductPrice.Text = p1.Price.ToString();
+
+                fileStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SoapWrite_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string path = @"D:\DP4\product.soap";
+                FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+                SoapFormatter soapFormatter = new SoapFormatter();
+                Product p1 = new Product();
+                p1.Id = Convert.ToInt32(ProductId.Text);
+                p1.Name = Productname.Text;
+                p1.Price = Convert.ToInt32(ProductPrice.Text);
+                soapFormatter.Serialize(fileStream, p1);
+                fileStream.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void SOAPRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.soap";
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+               SoapFormatter soapFormatter = new SoapFormatter();
+                Product p1 = new Product();
+                // we done explicit type casting from Object --> Product
+                p1 = (Product)soapFormatter.Deserialize(fileStream);
+                ProductId.Text = p1.Id.ToString();
+                Productname.Text = p1.Name;
+                ProductPrice.Text = p1.Price.ToString();
+
+                fileStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void JsonRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.json";
+                FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+                Product p1 = new Product();
+                p1.Id = Convert.ToInt32(ProductId.Text);
+                p1.Name = Productname.Text;
+                p1.Price = Convert.ToInt32(ProductPrice.Text);
+                JsonSerializer.Serialize<Product>(fileStream, p1);
+                fileStream.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\DP4\product.json";
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                Product p1 = new Product();
+                // we done explicit type casting from Object --> Product
+                p1 = JsonSerializer.Deserialize<Product>(fileStream);
+                ProductId.Text = p1.Id.ToString();
+                Productname.Text = p1.Name;
+                ProductPrice.Text = p1.Price.ToString();
+
+                fileStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
